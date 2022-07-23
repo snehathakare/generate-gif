4503d03b5ea812bc004a25c1056f4466eb794fb1
Sneha <snhthakare@gmail.com>
2022-07-23 18:48:29 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import RandomV1 from './components/RandomV1'
+import TagV1 from './components/TagV1'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Gif Application</h1>
+      <RandomV1 />
+      <TagV1 />
     </div>
   );
 }
